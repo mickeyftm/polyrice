b8ec480a5f3a4b532fe8dbca95f4e0fc825fdc40
brayden <braydenbuchner@gmail.com>
2021-06-10 01:24:08 +0200

    ..

---
+++ src/config/constants/contracts.ts
@@ -0,0 +1,31 @@
+export default {
+  cake: {
+    137: '0x67753F60c85f118D2eB5E2e30A778751Ff748179'
+  },
+  masterChef: {
+    137: '0x0C1ADC97e9A5027D81b6927F9d12D7079eDc0bA7',
+  },
+  weth: {
+    137: "0x7ceB23fD6bC0adD59E62ac25578270cFf1b9f619"
+  },
+  lottery: {
+    56: '',
+    97: '',
+  },
+  lotteryNFT: {
+    56: '',
+    97: '',
+  },
+  mulltiCall: {
+    137: '0x40524425bcb58Dc16f11746B0028e23aCDA1e128'
+  },
+  usdc: {
+    137: '0x2791Bca1f2de4661ED88A30C99A7a9449Aa84174'
+  },
+  wmatic: {
+    137: '0x0d500b1d8e8ef31e21c99d1db9a6444d3adf1270'
+  },
+  matic: {
+    137: '0x0000000000000000000000000000000000001010'
+  }
+}
